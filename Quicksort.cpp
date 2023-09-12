fd819b92334efd78e3221ed326481a0a7b513925
tabir <tabirijeffrey@gmail.com>
2023-09-12 17:10:17 +0200

    feat(Quicksort): add quicksort with countsort algorithm

--- Quicksort.cpp
+++ Quicksort.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <chrono>
 #include <bits/stdc++.h>
+
+
+
+void quickSortTest(int n);
+
 using namespace std;
 using namespace std::chrono;
 
@@ -48,29 +53,14 @@ int medianSort(int *table, int leftEnd, int rightEnd) {
     return middlePos;
 }
 
-void findLargestNumber(int table, int tableLength) {
-
-}
-int split2(int *table, int leftEnd, int rightEnd) {
-    int iv;
-    int ih;
-    int divideNumber = medianSort(table, leftEnd, rightEnd);
-    int dv = table[divideNumber];
-    switchPlace(&table[divideNumber], &table[rightEnd - 1]);
-
-    for (iv = leftEnd, ih = rightEnd - 1;;) {
-        while (table[++iv] < dv);
-        while (table[--ih] > dv);
-        if (iv >= ih) break;
-        switchPlace(&table[iv], &table[ih]);
-    }
-
-    switchPlace(&table[iv], &table[rightEnd-1]);
-
-    return iv;
-}
-
-
+/**
+ * Divided arrays into appropriate sub-arrays.
+ *
+ * @param table the table to be further divided
+ * @param leftEnd of the table.
+ * @param rightEnd of the table
+ * @return returns an integer, which represents the index in where the array should be divided.
+ */
 int split(int *table, int leftEnd, int rightEnd) {
     int iv;
     int ih;
@@ -90,7 +80,12 @@ int split(int *table, int leftEnd, int rightEnd) {
     return iv;
 }
 
-
+/**
+ * Sorting algorithm for lots of data.
+ * @param table represents the table in  which we are going to sort
+ * @param leftEnd of the table.
+ * @param rightEnd right en
+ */
 void quickSort(int *table, int leftEnd, int rightEnd) {
     if (rightEnd - leftEnd > 2) {
         int dividingPosition = split(table, leftEnd, rightEnd);
@@ -99,115 +94,407 @@ void quickSort(int *table, int leftEnd, int rightEnd) {
     } else medianSort(table, leftEnd, rightEnd);
 }
 
+
+/**
+ * A s
+ * @param
+ * @return
+ */
 int * createArray(int n) {
+
     srand((int)time(0));
 
     int *table = new int [n];
     for (int i = 0; i < n; ++i) {
-        table[i] = rand() % 10;
+        table[i] = rand() % 30;
     }
 
     return table;
 }
 
+int * createArrayWithHugeSpread(int n) {
+
+    srand((int)time(0));
+
+    int *table = new int [n];
+    for (int i = 0; i < n; ++i) {
+        table[i] = rand() % 500;
+    }
+
+    return table;
+}
+
+
+int * createArrayOfDuplicates(int n) {
+
+    srand((int)time(0));
+
+    int *table = new int [n];
+    for (int i = 0; i < n; ++i) {
+        if (i % 2 == 0) {
+            table[i] = rand() % 30;
+        } else {
+            table[i] = 42;
+        }
+    }
+
+    return table;
+}
+
+
+/**
+ * Checks if the given table is sorted such as 1..2..3..4
+ *
+ * @param table the table to be checked.
+ * @param tableLength the amount of elements in the table.
+ * @return true or false statement.
+ */
 bool checkSorted(int *table, int tableLength) {
+
     for (int i = 1; i < tableLength - 1; i++) {
         if (table[i] < table[i-1]) {
             return false;
         }
     }
+
     return true;
+
 }
 
+
+/**
+ * Calculates a sum of a given table.
+ *
+ * @param table in the table to be checked.
+ * @param tableLength the amount of elements in the table.
+ * @return the integer which is the sum.
+ */
 int sumCalculate(int *table, int tableLength) {
+
     int sum = 0;
+
     for (int i = 0; i < tableLength; i++) {
         sum += table[i];
     }
+
+
     return sum;
 }
 
-void countSort(int n, int inputTable[n], int outputTable[n], int k) {
 
-    int ht[k+1];
+/**
+ * A count sort method which sorts numbers in chronological order.
+ *
+ * @param tableLength the amount of elements in the array.
+ * @param array in the array to be checked.
+ * @param max_value the maximum value that exists in the given array.
+ */
+void countSort(int tableLength, int *array, int max_value, int leftEnd, int rightEnd) {
+    int count[max_value + 1];
 
-    for (int i = 0; i <= k; ++i) {
-        ht[i] = 0;
-
+    for (int i = 0; i <= max_value; ++i) {
+        count[i] = 0;
+    }
+    for (int i = leftEnd; i <= rightEnd; ++i) {
+        ++count[array[i]];
     }
 
-    for (int i = 0; i < n; ++i)  {
-        ++ht [inputTable[i]];
+    int index = leftEnd;
+    for (int i = 0; i <= max_value; ++i) {
+        while(count[i] > 0) {
+            array[index] = i;
+            index++;
+            count[i]--;
+        }
     }
 
-    for (int i = n - 1; i >= 0; --i) {
-        outputTable[--ht[inputTable[i]]] = inputTable[i];
-    }
+
 }
 
-void quickSortWithOptimization(int *table, int leftEnd, int rightEnd) {
+void quickSortWithOptimization(int *array, int leftEnd, int rightEnd) {
 
-    int max = *max_element(array, 1, 2);
-    int min = table[leftEnd];
+    int arraySize = rightEnd - leftEnd;
 
-    if (rightEnd - leftEnd > 2) {
-        int dividingPosition = split2(table, leftEnd, rightEnd);
-        quickSortWithOptimization(table, leftEnd, dividingPosition - 1);
-        quickSortWithOptimization(table, dividingPosition + 1, rightEnd);
-    } else medianSort(table, leftEnd, rightEnd);
+    if (array[rightEnd + 1] - array[leftEnd - 1] < rightEnd - leftEnd) {
+        countSort(arraySize, array, array[rightEnd + 1], leftEnd, rightEnd);
+    }
+
+    else if (rightEnd - leftEnd > 2) {
+
+        int dividingPosition = split(array, leftEnd, rightEnd);
+        quickSortWithOptimization(array, leftEnd, dividingPosition - 1);
+        quickSortWithOptimization(array, dividingPosition + 1, rightEnd);
+
+    } else {
+        medianSort(array, leftEnd, rightEnd);
+    }
+
 }
+
+
+int findMaxIndexInArray(int arraySize, int *array) {
+    int max = 0;
+    int index = 0;
+
+    for (int i = 0; i < arraySize; ++i) {
+        if (array[i] > max) {
+            max = array[i];
+            index = i;
+        }
+        
+    }
+    return index;
+}
+
+int findMinIndexInArray(int arraySize, int *array) {
+
+    int min = INT_MAX;
+    int index = 0;
+
+    for (int i = 0; i < arraySize; ++i) {
+        if (array[i] < min) {
+            min = array[i];
+            index = i;
+        }
+
+    }
+    return index;
+}
+
+void confirmArray(int n, int *array, int sum) {
+
+    if (checkSorted(array, n)) {
+        cout << "INFO: " << "The table is sorted." << endl;
+    } else {
+        cout << "INFO: " << "The table is not sorted." << endl;
+    }
+
+    if (sum == sumCalculate(array, n)) {
+        cout << "INFO: " << "No overwriting has happened." << endl;
+    } else {
+        cout << "INFO: " << "Overwriting has happened." << endl;
+    }
+
+    cout << endl;
+}
+
+void optimizedQuickSort(int n) {
+    int *array = createArray(n);
+    int sum = sumCalculate(array, n);
+    switchPlace(&array[n-1], &array[findMaxIndexInArray(n,array)]);
+    switchPlace(&array[0], &array[findMinIndexInArray(n, array)]);
+    auto start = high_resolution_clock::now();
+    quickSortWithOptimization(array, 1, n-2);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time : " <<  duration.count() << "ms" << endl;
+    confirmArray(n, array, sum);
+    delete array;
+}
+
+
+void optimizedQuickSortWithDuplicates(int n) {
+    int *array = createArrayOfDuplicates(n);
+    int sum = sumCalculate(array, n);
+    switchPlace(&array[n-1], &array[findMaxIndexInArray(n,array)]);
+    switchPlace(&array[0], &array[findMinIndexInArray(n, array)]);
+    auto start = high_resolution_clock::now();
+    quickSortWithOptimization(array, 1, n-2);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time : " <<  duration.count() << "ms" << endl;
+    confirmArray(n, array, sum);
+    delete array;
+}
+
+
+void quickSortWithHugeSpread(int n) {
+    int *array = createArrayWithHugeSpread(n);
+    int sum = sumCalculate(array, n);
+    auto start = high_resolution_clock::now();
+    quickSort(array, 0, n-1);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time : " <<  duration.count() << "ms" << endl;
+    confirmArray(n, array, sum);
+
+    delete array;
+}
+
+void optimizedQuickSortWithHugeSpread(int n) {
+    int *array = createArrayWithHugeSpread(n);
+    int sum = sumCalculate(array, n);
+    switchPlace(&array[n-1], &array[findMaxIndexInArray(n,array)]);
+    switchPlace(&array[0], &array[findMinIndexInArray(n, array)]);
+    auto start = high_resolution_clock::now();
+    quickSortWithOptimization(array, 1, n-2);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time : " <<  duration.count() << "ms" << endl;
+    confirmArray(n, array, sum);
+    delete array;
+}
+
+void quickSortTestWithDuplicates(int n) {
+    int *array = createArrayOfDuplicates(n);
+    int sum = sumCalculate(array, n);
+    auto start = high_resolution_clock::now();
+    quickSort(array, 0, n-1);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time : " <<  duration.count() << "ms" << endl;
+    confirmArray(n, array, sum);
+    delete array;
+}
+
+void quickSortOfASortedArray(int n) {
+    int *array = createArray(n);
+
+    int sum = sumCalculate(array, n);
+    quickSort(array, 0, n-1);
+
+    auto start = high_resolution_clock::now();
+    quickSort(array, 0, n-1);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time : " <<  duration.count() << "ms" << endl;
+    confirmArray(n, array, sum);
+
+    delete array;
+}
+
+
+void optimizedQuickSortOfASortedArray(int n) {
+    int *array = createArray(n);
+
+    int sum = sumCalculate(array, n);
+
+    switchPlace(&array[n-1], &array[findMaxIndexInArray(n,array)]);
+    switchPlace(&array[0], &array[findMinIndexInArray(n, array)]);
+    auto start = high_resolution_clock::now();
+    quickSortWithOptimization(array, 1, n-2);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time : " <<  duration.count() << "ms" << endl;
+    confirmArray(n, array, sum);
+
+    cout << "SORTING SECOND TIME." << endl;
+    start = high_resolution_clock::now();
+    quickSortWithOptimization(array, 1, n-2);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time : " <<  duration.count() << "ms" << endl;
+    confirmArray(n, array, sum);
+
+    delete array;
+}
+
+
+
 
 int main() {
 
-    int n = 100000000;
+    //Specifies array size.
+    int n = 20000000;
+
+    cout << "QUICKSORT WITH COUNTSORT PHASE" << endl;
+    cout << n << " ELEMENTS." << endl;
+    cout << "==============================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 5; ++i) {
+        optimizedQuickSort(n);
+    }
+
+    cout << "QUICKSORT WITH COUNTSORT PHASE" << endl;
+    cout << n << " ELEMENTS." << endl;
+    cout << "ARRAY WITH IMMENSE DUPLICATES." << endl;
+    cout << "===============================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 5; ++i) {
+        optimizedQuickSortWithDuplicates(n);
+    }
+
+    cout << "QUICKSORT WITH COUNTSORT PHASE" << endl;
+    cout << n << " ELEMENTS." << endl;
+    cout << "SORT A SORTED ARRAY." << endl;
+    cout << "===============================" << endl;
+    cout << endl;
+    optimizedQuickSortOfASortedArray(n);
+
+    cout << "QUICKSORT WITH COUNTSORT PHASE" << endl;
+    cout << n << " ELEMENTS." << endl;
+    cout << "1 - 499 IN SPREAD." << endl;
+    cout << "===============================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 3; ++i) {
+        optimizedQuickSortWithHugeSpread(n);
+    }
+
+
+    cout << "QUICKSORT PHASE" << endl;
+    cout << n << " ELEMENTS." << endl;
+    cout << "===============================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 5; ++i) {
+        quickSortTest(n);
+    }
+
+    cout << "QUICKSORT PHASE" << endl;
+    cout << n << " ELEMENTS." << endl;
+    cout << "ARRAY WITH IMMENSE DUPLICATES." << endl;
+    cout << "===============================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 5; ++i) {
+        quickSortTestWithDuplicates(n);
+    }
+
+    cout << "QUICKSORT WITH PHASE" << endl;
+    cout << n << " ELEMENTS." << endl;
+    cout << "SORT A SORTED ARRAY." << endl;
+    cout << "===============================" << endl;
+    cout << endl;
+    for (int i = 0; i < 5; ++i) {
+        quickSortOfASortedArray(n);
+    }
+
+    cout << "QUICKSORT WITH PHASE" << endl;
+    cout << n << " ELEMENTS." << endl;
+    cout << "1 - 499 IN SPREAD." << endl;
+    cout << "===============================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 3; ++i) {
+        quickSortWithHugeSpread(n);
+    }
+
+    return 0;
+}
+
+void quickSortTest(int n) {
     int *array = createArray(n);
+
     int sum = sumCalculate(array, n);
 
     auto start = high_resolution_clock::now();
     quickSort(array, 0, n-1);
     auto stop = high_resolution_clock::now();
-
-    if (checkSorted(array, n)) {
-        cout << "The table is sorted." << endl;
-    } else {
-        cout << "The table is not sorted." << endl;
-    }
-
-    if (sum == sumCalculate(array, n)) {
-        cout << "No overwriting has happened." << endl;
-    } else {
-        cout << "Overwriting has happened." << endl;
-    }
-
-    auto duration = duration_cast<seconds>(stop - start);
-    cout << duration.count() << "sec" << endl;
-
-
-    auto start2 = high_resolution_clock::now();
-    quickSort(array, 0, n-1);
-    auto stop2 = high_resolution_clock::now();
-
-    if (checkSorted(array, n)) {
-        cout << "The second table is sorted." << endl;
-    } else {
-        cout << "The second table is not sorted." << endl;
-    }
-
-    if (sum == sumCalculate(array, n)) {
-        cout << "No overwriting has happened." << endl;
-    } else {
-        cout << "Overwriting has happened." << endl;
-    }
-
-    auto duration2 = duration_cast<seconds>(stop2 - start2);
-    cout << duration2.count() << "sec" << endl;
-
-
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time : " <<  duration.count() << "ms" << endl;
+    confirmArray(n, array, sum);
 
     delete array;
-
-    return 0;
 }
+
+
+
+
+
+
 
 
 
